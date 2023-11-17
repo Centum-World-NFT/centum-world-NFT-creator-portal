2ef19d4d2bf8f909a6aa35eb40e60fcf0498d147
lakhan <lakhanmahto3676@gmail.com>
2023-11-17 18:20:14 +0530

    api integrate for subscrine

--- src/pages/Subscribe/Subscriber.jsx
+++ src/pages/Subscribe/Subscriber.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
   Filtercard,
   SubscriberCard,
@@ -6,29 +6,39 @@ import {
   SubscriberDetails,
   SubscriberHeading,
   Warrper,
-} from "./SubscriberStyle";
-import { Button, Divider, TextField, Typography } from "@mui/material";
-import image from "../../utils/cardimages/image.jpg";
-import { FilterIcon, INR } from "../../utils/icons";
-import BlockIcon from "@mui/icons-material/Block";
-import BlockModal from "../../components/common/Modal/BlockModal";
+} from './SubscriberStyle';
+import { useDispatch, useSelector } from 'react-redux';
+import { subcriberDetails } from '@/redux/slices/subscribeSlice';
+import { Button, Divider, TextField, Typography } from '@mui/material';
+import image from '../../utils/cardimages/image.jpg';
+import { FilterIcon, INR } from '../../utils/icons';
+import BlockIcon from '@mui/icons-material/Block';
+import BlockModal from '../../components/common/Modal/BlockModal';
 
 const Subscriber = () => {
-    const [isModalOpen, setModalOpen] = useState(false);
+  const subscriberState = useSelector((state) => state.subscriber);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const value = {
+    creatorId: '65530e583c3030d2f346a1cc',
+  };
 
-    const handleButtonClick = () => {
-      setModalOpen(true);
-    };
-  
-    const handleCloseModal = () => {
-      setModalOpen(false);
-    };
-  
+  const handleButtonClick = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(subcriberDetails(value));
+  }, []);
+
   return (
     <Warrper>
-      <Typography
-        style={{ fontSize: "30px", fontWeight: "700", marginBottom: "15px" }}
-      >
+      <Typography style={{ fontSize: '30px', fontWeight: '700', marginBottom: '15px' }}>
         Our subscribers
       </Typography>
       <Filtercard>
@@ -37,285 +47,63 @@ const Subscriber = () => {
           id="input-with-icon-textfield"
           placeholder="Search by subscriber"
           variant="standard"
-          style={{ marginLeft: "10px" }}
+          style={{ marginLeft: '10px' }}
         />
       </Filtercard>
       <SubscriberContainer>
-        <SubscriberCard>
-          <SubscriberHeading>
-            <Typography style={{ fontSize: "18px", fontWeight: "700" }}>
-              Profile
-            </Typography>
-            <img
-              src={image}
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-            />
-          </SubscriberHeading>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>First name</Typography>
-            <Typography>Arjun</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Last name</Typography>
-            <Typography>Mandal</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Subscribe</Typography>
-            <Typography>AWS</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Price</Typography>
-            <Typography>
-              <INR />
-              9999
-            </Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Joining date</Typography>
-            <Typography>12/11/2023</Typography>
-          </SubscriberDetails>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<BlockIcon />}
-            fullWidth
-            onClick={handleButtonClick}
-          >
-            Block
-          </Button>
-        </SubscriberCard>
-
-        <SubscriberCard>
-          <SubscriberHeading>
-            <Typography style={{ fontSize: "18px", fontWeight: "700" }}>
-              Profile
-            </Typography>
-            <img
-              src={image}
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-            />
-          </SubscriberHeading>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>First name</Typography>
-            <Typography>Givind</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Last name</Typography>
-            <Typography>Kumar</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Subscribe</Typography>
-            <Typography>Angular</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Price</Typography>
-            <Typography>
-              <INR />
-              4555
-            </Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Joining date</Typography>
-            <Typography>23/11/2023</Typography>
-          </SubscriberDetails>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<BlockIcon />}
-            fullWidth
-            onClick={handleButtonClick}
-          >
-            Block
-          </Button>
-        </SubscriberCard>
-
-        <SubscriberCard>
-          <SubscriberHeading>
-            <Typography style={{ fontSize: "18px", fontWeight: "700" }}>
-              Profile
-            </Typography>
-            <img
-              src={image}
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-            />
-          </SubscriberHeading>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>First name</Typography>
-            <Typography>Rahul</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Last name</Typography>
-            <Typography>Kumar</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Subscribe</Typography>
-            <Typography>React JS</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Price</Typography>
-            <Typography>
-              <INR />
-              11,999
-            </Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Joining date</Typography>
-            <Typography>12/12/2023</Typography>
-          </SubscriberDetails>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<BlockIcon />}
-            fullWidth
-            onClick={handleButtonClick}
-          >
-            Block
-          </Button>
-        </SubscriberCard>
-
-        {/* ------------------- */}
-        <SubscriberCard>
-          <SubscriberHeading>
-            <Typography style={{ fontSize: "18px", fontWeight: "700" }}>
-              Profile
-            </Typography>
-            <img
-              src={image}
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-            />
-          </SubscriberHeading>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>First name</Typography>
-            <Typography>Arjun</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Last name</Typography>
-            <Typography>Mandal</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Subscribe</Typography>
-            <Typography>AWS</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Price</Typography>
-            <Typography>
-              <INR />
-              9999
-            </Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Joining date</Typography>
-            <Typography>12/11/2023</Typography>
-          </SubscriberDetails>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<BlockIcon />}
-            fullWidth
-            onClick={handleButtonClick}
-          >
-            Block
-          </Button>
-        </SubscriberCard>
-
-        <SubscriberCard>
-          <SubscriberHeading>
-            <Typography style={{ fontSize: "18px", fontWeight: "700" }}>
-              Profile
-            </Typography>
-            <img
-              src={image}
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-            />
-          </SubscriberHeading>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>First name</Typography>
-            <Typography>Givind</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Last name</Typography>
-            <Typography>Kumar</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Subscribe</Typography>
-            <Typography>Angular</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Price</Typography>
-            <Typography>
-              <INR />
-              4555
-            </Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Joining date</Typography>
-            <Typography>23/11/2023</Typography>
-          </SubscriberDetails>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<BlockIcon />}
-            fullWidth
-            onClick={handleButtonClick}
-          >
-            Block
-          </Button>
-        </SubscriberCard>
-
-        <SubscriberCard>
-          <SubscriberHeading>
-            <Typography style={{ fontSize: "18px", fontWeight: "700" }}>
-              Profile
-            </Typography>
-            <img
-              src={image}
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-            />
-          </SubscriberHeading>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>First name</Typography>
-            <Typography>Rahul</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Last name</Typography>
-            <Typography>Kumar</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Subscribe</Typography>
-            <Typography>React JS</Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Price</Typography>
-            <Typography>
-              <INR />
-              11,999
-            </Typography>
-          </SubscriberDetails>
-          <SubscriberDetails>
-            <Typography style={{ color: "#828792" }}>Joining date</Typography>
-            <Typography>12/12/2023</Typography>
-          </SubscriberDetails>
-          <Divider sx={{ color: "red", margin: "10px 0" }} />
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<BlockIcon />}
-            fullWidth
-          >
-            Block
-          </Button>
-        </SubscriberCard>
+        {subscriberState.isLoading ? (
+          <Typography>Loading...</Typography>
+        ) : subscriberState.isError ? (
+          <Typography>Error: {subscriberState.isError}</Typography>
+        ) : (
+          subscriberState.data &&
+          subscriberState.data.data.map((subscriber) => (
+            <SubscriberCard key={subscriber._id}>
+              <SubscriberHeading>
+                <Typography style={{ fontSize: '18px', fontWeight: '700' }}>Profile</Typography>
+                <img
+                  src={image}
+                  style={{ width: '30px', height: '30px', borderRadius: '50%' }}
+                  alt="Profile Pic"
+                />
+              </SubscriberHeading>
+              <Divider sx={{ color: 'red', margin: '10px 0' }} />
+              <SubscriberDetails>
+                <Typography style={{ color: '#828792' }}>First name</Typography>
+                <Typography>{subscriber.firstName}</Typography>
+              </SubscriberDetails>
+              <SubscriberDetails>
+                <Typography style={{ color: '#828792' }}>Last name</Typography>
+                <Typography>{subscriber.lastName}</Typography>
+              </SubscriberDetails>
+              <SubscriberDetails>
+                <Typography style={{ color: '#828792' }}>Subscribe</Typography>
+                <Typography>{subscriber.subscribe}</Typography>
+              </SubscriberDetails>
+              <SubscriberDetails>
+                <Typography style={{ color: '#828792' }}>Price</Typography>
+                <Typography>
+                  <INR />
+                  {subscriber.price}
+                </Typography>
+              </SubscriberDetails>
+              <SubscriberDetails>
+                <Typography style={{ color: '#828792' }}>Joining date</Typography>
+                <Typography>{subscriber.joiningDate}</Typography>
+              </SubscriberDetails>
+              <Divider sx={{ color: 'red', margin: '10px 0' }} />
+              <Button
+                component="label"
+                variant="contained"
+                startIcon={<BlockIcon />}
+                fullWidth
+                onClick={handleButtonClick}
+              >
+                Block
+              </Button>
+            </SubscriberCard>
+          ))
+        )}
       </SubscriberContainer>
 
       <BlockModal isOpen={isModalOpen} handleClose={handleCloseModal} />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/auth";
-import formReducer from "./slices/formSlice"
+import formReducer from "./slices/formSlice"; 
+import subscribeReducer from "./slices/subscribeSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    subscriber: subscribeReducer
   },
 });

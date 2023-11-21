65d5c63bc629d59cbd74e09fcc01d95eba33c91c
lakhan <lakhanmahto3676@gmail.com>
2023-11-21 17:53:48 +0530

    block or unblock

--- src/pages/Subscribe/Subscriber.jsx
+++ src/pages/Subscribe/Subscriber.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   Filtercard,
   SubscriberCard,
@@ -6,23 +6,29 @@ import {
   SubscriberDetails,
   SubscriberHeading,
   Warrper,
-} from './SubscriberStyle';
-import { useDispatch, useSelector } from 'react-redux';
-import { subcriberDetails } from '@/redux/slices/subscribeSlice';
-import { Button, Divider, TextField, Typography } from '@mui/material';
-import image from '../../utils/cardimages/image.jpg';
-import { FilterIcon, INR } from '../../utils/icons';
-import BlockIcon from '@mui/icons-material/Block';
-import BlockModal from '../../components/common/Modal/BlockModal';
+} from "./SubscriberStyle";
+import { useDispatch, useSelector } from "react-redux";
+import { subcriberDetails } from "@/redux/slices/subscribeSlice";
+import { Button, Divider, TextField, Typography } from "@mui/material";
+import image from "../../utils/cardimages/image.jpg";
+import { FilterIcon, INR } from "../../utils/icons";
+import BlockIcon from "@mui/icons-material/Block";
+import CancelIcon from "@mui/icons-material/Cancel";
+import BlockModal from "../../components/common/Modal/BlockModal";
 
 const Subscriber = () => {
   const subscriberState = useSelector((state) => state.subscriber);
+  const blockState = useSelector((state) => state.block);
   const [isModalOpen, setModalOpen] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [block, setBlock] = useState(false);
   const value = {
-    creatorId: '65530e583c3030d2f346a1cc',
+    creatorId: "65530e583c3030d2f346a1cc",
   };
 
-  const handleButtonClick = () => {
+  const handleButtonClick = (subscriber) => {
+    setBlock(subscriber.isBlocked);
+    setUserId(subscriber._id);
     setModalOpen(true);
   };
 
@@ -36,9 +42,16 @@ const Subscriber = () => {
     dispatch(subcriberDetails(value));
   }, []);
 
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
   return (
     <Warrper>
-      <Typography style={{ fontSize: '30px', fontWeight: '700', marginBottom: '15px' }}>
+      <Typography
+        style={{ fontSize: "30px", fontWeight: "700", marginBottom: "15px" }}
+      >
         Our subscribers
       </Typography>
       <Filtercard>
@@ -47,7 +60,7 @@ const Subscriber = () => {
           id="input-with-icon-textfield"
           placeholder="Search by subscriber"
           variant="standard"
-          style={{ marginLeft: '10px' }}
+          style={{ marginLeft: "10px" }}
         />
       </Filtercard>
       <SubscriberContainer>
@@ -60,53 +73,65 @@ const Subscriber = () => {
           subscriberState.data.data.map((subscriber) => (
             <SubscriberCard key={subscriber._id}>
               <SubscriberHeading>
-                <Typography style={{ fontSize: '18px', fontWeight: '700' }}>Profile</Typography>
+                <Typography style={{ fontSize: "18px", fontWeight: "700" }}>
+                  Profile
+                </Typography>
                 <img
                   src={image}
-                  style={{ width: '30px', height: '30px', borderRadius: '50%' }}
+                  style={{ width: "30px", height: "30px", borderRadius: "50%" }}
                   alt="Profile Pic"
                 />
               </SubscriberHeading>
-              <Divider sx={{ color: 'red', margin: '10px 0' }} />
+              <Divider sx={{ color: "red", margin: "10px 0" }} />
               <SubscriberDetails>
-                <Typography style={{ color: '#828792' }}>First name</Typography>
+                <Typography style={{ color: "#828792" }}>First name</Typography>
                 <Typography>{subscriber.firstName}</Typography>
               </SubscriberDetails>
               <SubscriberDetails>
-                <Typography style={{ color: '#828792' }}>Last name</Typography>
+                <Typography style={{ color: "#828792" }}>Last name</Typography>
                 <Typography>{subscriber.lastName}</Typography>
               </SubscriberDetails>
               <SubscriberDetails>
-                <Typography style={{ color: '#828792' }}>Subscribe</Typography>
+                <Typography style={{ color: "#828792" }}>Subscribe</Typography>
                 <Typography>{subscriber.subscribe}</Typography>
               </SubscriberDetails>
               <SubscriberDetails>
-                <Typography style={{ color: '#828792' }}>Price</Typography>
+                <Typography style={{ color: "#828792" }}>Price</Typography>
                 <Typography>
                   <INR />
                   {subscriber.price}
                 </Typography>
               </SubscriberDetails>
               <SubscriberDetails>
-                <Typography style={{ color: '#828792' }}>Joining date</Typography>
-                <Typography>{subscriber.joiningDate}</Typography>
+                <Typography style={{ color: "#828792" }}>
+                  Joining date
+                </Typography>
+                <Typography>{formatDate(subscriber.joiningDate)}</Typography>
               </SubscriberDetails>
-              <Divider sx={{ color: 'red', margin: '10px 0' }} />
+              <Divider sx={{ color: "red", margin: "10px 0" }} />
               <Button
                 component="label"
                 variant="contained"
-                startIcon={<BlockIcon />}
+                startIcon={
+                  subscriber.isBlocked ? <CancelIcon /> : <BlockIcon />
+                }
                 fullWidth
-                onClick={handleButtonClick}
+                onClick={() => handleButtonClick(subscriber)}
               >
-                Block
+                {subscriber.isBlocked ? "Unblock" : "Block"}
               </Button>
             </SubscriberCard>
           ))
         )}
       </SubscriberContainer>
 
-      <BlockModal isOpen={isModalOpen} handleClose={handleCloseModal} />
+      <BlockModal
+        isOpen={isModalOpen}
+        handleClose={handleCloseModal}
+        id={userId}
+        block={block}
+        onSuccess={() => dispatch(subcriberDetails(value))}
+      />
     </Warrper>
   );
 };
---
+++ src/redux/slices/blockUserSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { blockUserApi } from "../apis/userblock";
+
+export const  blockUser = createAsyncThunk("block", async (payload) => {
+    console.log(payload)
+    try{
+        const response = await blockUserApi(payload);
+        console.log(response.data)
+        return response.data;
+    }catch(error) {
+        console.log(error.message)
+    }
+})
+
+const initialState = {
+    isLoading: true,
+    data: null,
+    isError: false,
+  };
+  const blockuserSlice = createSlice({
+    name: "block",
+    initialState,
+    extraReducers: (builder) => {
+      builder.addCase(blockUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+        state.isError = false;
+      });
+      builder.addCase(blockUser.pending, (state, action) => {
+        state.isLoading = true;
+        state.data = null;
+        state.isError = false;
+      });
+      builder.addCase(blockUser.rejected, (state, action) => {
+        state.isLoading = false;
+        console.log("Error", action.error.message);
+        state.isError = true;
+      });
+    },
+  });
+  
+  export default blockuserSlice.reducer;
